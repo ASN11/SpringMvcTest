873ad22db592d1004d1e7753651265bb697b3fdd
Stephan Ricotta <stephanr@student.21-school.ru>
2023-07-27 17:08:54 +0300

    поправил на java конфиг

--- src/main/java/school21/controllers/HelloController.java
+++ src/main/java/school21/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package school21;
+package school21.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +8,13 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        System.out.println("Controller JSP is working");
+        System.out.println("Controller is working");
         return "hello_world";
+    }
+
+    @GetMapping("/bye")
+    public String sayBye() {
+        System.out.println("Controller is working");
+        return "bye";
     }
 }
